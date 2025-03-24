4cd1db824a319e3a59b5fd9a759f8467642e10ce
shubham79a <shubh79.nolan@gmail.com>
2025-03-25 02:30:18 +0530

    added filter jobs

--- Client/src/components/FilterCard.jsx
+++ Client/src/components/FilterCard.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RadioGroup, RadioGroupItem } from './ui/radio-group'
 import { Label } from './ui/label'
+import { useDispatch } from 'react-redux'
+import { setSearchedQuery } from '@/redux/jobSlice'
 
 // location
 // salary
@@ -13,7 +15,7 @@ const filterData = [
     },
     {
         filterType: "Industry",
-        array: ["Frontend Developer", "Banglore", "Mumbai", "Ranchi", "Patna"]
+        array: ["Frontend Developer", "Backend Developer", "Full Stack Developer", "Data Scientist", "Machine Learning"]
     },
     {
         filterType: "Salary",
@@ -23,22 +25,37 @@ const filterData = [
 
 
 const FilterCard = () => {
+
+    const [selectedValue, setSelectedValue] = useState("");
+    const dispatch = useDispatch();
+
+    const changeHandler = (value) => {
+        setSelectedValue(value);
+    }
+
+
+    useEffect(() => {
+        // console.log(selectedValue);
+        dispatch(setSearchedQuery(selectedValue));
+    }, [selectedValue])
+
     return (
         <div className='w-full bg-white p-3 rounded-md'>
             <h1 className='font-bold text-lg'>Filter Jobs</h1>
             <hr className='mt-3' />
 
-            <RadioGroup>
+            <RadioGroup value={selectedValue} onValueChange={changeHandler}>
                 {
                     filterData.map((data, index) => (
                         <div key={index}>
                             <h1 className='font-bold text-lg'>{data.filterType}</h1>
                             {
-                                data.array.map((item, index) => {
+                                data.array.map((item, idx) => {
+                                    const itemId = `r${index - idx}`
                                     return (
-                                        <div className='flex items-center spac-x-2 my-2' key={index}>
-                                            <RadioGroupItem value={item} />
-                                            <Label>{item}</Label>
+                                        <div className='flex items-center spac-x-2 my-2' key={idx}>
+                                            <RadioGroupItem value={item} id={itemId} />
+                                            <Label htmlFor={itemId}>{item}</Label>
                                         </div>
                                     )
 
--- Client/src/components/Jobs.jsx
+++ Client/src/components/Jobs.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './shared/Navbar'
 import Footer from './shared/Footer'
 import FilterCard from './FilterCard'
 import Job from './Job'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setSearchedQuery } from '@/redux/jobSlice'
+import { motion } from 'framer-motion'
 
 
 // const jobs = [
@@ -13,7 +15,31 @@ import { useSelector } from 'react-redux'
 
 function Jobs() {
 
-    const { allJobs } = useSelector(state => state.job)
+    const dispatch = useDispatch();
+    const { allJobs, searchedQuery } = useSelector(state => state.job);
+
+    const [filterJobs, setFilterJobs] = useState(allJobs);
+
+
+    useEffect(() => {
+
+        if (searchedQuery) {
+            const filteredJob = allJobs.filter((job) => {
+                return job?.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+                    job?.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+                    job?.location.toLowerCase().includes(searchedQuery.toLowerCase())
+                // job?.company.toLowerCase().includes(searchedQuery.toLowerCase())
+            })
+            setFilterJobs(filteredJob)
+            // dispatch(setSearchedQuery(""));
+
+
+        }
+        // else {
+        //     setFilterJobs(allJobs)
+        // }
+
+    }, [allJobs, searchedQuery])
 
     return (
         <>
@@ -27,14 +53,19 @@ function Jobs() {
 
 
                     {
-                        allJobs.length <= 0 ? <span>No Jobs Found</span> :
+                        filterJobs.length <= 0 ? <span>No Jobs Found</span> :
                             <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
                                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                                     {
-                                        allJobs.map((job) => (
-                                            <div key={job._id}>
+                                        filterJobs.map((job) => (
+                                            <motion.div
+                                                initial={{ opacity: 0, x: 100 }}
+                                                animate={{ opacity: 1, x: 0 }}
+                                                exit={{ opacity: 0, x: -100 }}
+                                                transition={{ duration: 0.3 }}
+                                                key={job._id}>
                                                 <Job job={job} />
-                                            </div>
+                                            </motion.div>
                                         ))
                                     }
                                 </div>
