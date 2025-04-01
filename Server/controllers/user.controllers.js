5f4f2230c7034680cb649bee7b31435ea5332191
shubham79a <shubh79.nolan@gmail.com>
2025-04-01 22:02:47 +0530

    cookies

--- Server/controllers/user.controllers.js
+++ Server/controllers/user.controllers.js
@@ -102,7 +102,10 @@ export const login = async (req, res) => {
         }
 
         return res.status(200).cookie("token", token,
-            { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' },
+            {
+                maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none',
+                secure: isProduction
+            },
         ).json({
             message: `Welcome back ${user.fullname}`,
             user,
