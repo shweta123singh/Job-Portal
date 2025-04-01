51023c100bbaf95e6063080c4d014bf41472923e
shubham79a <shubh79.nolan@gmail.com>
2025-04-01 22:13:11 +0530

    production

--- Server/controllers/user.controllers.js
+++ Server/controllers/user.controllers.js
@@ -104,7 +104,7 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token,
             {
                 maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none',
-                secure: isProduction
+                secure: process.env.NODE_ENV === "production"
             },
         ).json({
             message: `Welcome back ${user.fullname}`,
