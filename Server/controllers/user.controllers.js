e5d46d81e05d76b0137499ab83de64ee9d808e4d
Shubham Kumar <146449583+shubham79a@users.noreply.github.com>
2025-04-06 02:32:28 +0530

    Update user.controllers.js

--- Server/controllers/user.controllers.js
+++ Server/controllers/user.controllers.js
@@ -103,8 +103,10 @@ export const login = async (req, res) => {
 
         return res.status(200).cookie("token", token,
             {
-                maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none',
-                secure: process.env.NODE_ENV === "production"
+                maxAge: 1 * 24 * 60 * 60 * 1000, 
+                httpOnly: true,
+                sameSite: 'none',
+                secure: true
             },
         ).json({
             message: `Welcome back ${user.fullname}`,
